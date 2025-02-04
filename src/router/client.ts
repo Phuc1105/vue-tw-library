1aee5f4214c7fa01738915cb6e2ca3574d8a72ba
Your Name <you@example.com>
2025-02-04 14:27:39 +0700

    #10 - zane09 - [theme] - component:Thiết kế giao diện trang sản phẩm chi tiết

--- src/router/client.ts
+++ src/router/client.ts
@@ -1,5 +1,6 @@
 import Banner from '@/views/banner/Banner.vue';
 import LayoutClient from '@/layouts/client.vue';
+import ProductDetail from '@/components/productdetail/ProductDetail.vue';
 
 const client = [
     {
@@ -10,6 +11,11 @@ const client = [
                 path: '',
                 name: 'component-banner',
                 component: () => Banner,
+            },
+            {
+                path: '/productdetail',
+                name: 'component-=productdetail',
+                component: () => ProductDetail,
             }
         ]
     }
